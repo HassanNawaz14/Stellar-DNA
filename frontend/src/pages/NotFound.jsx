df0a88107140b114da01437165a74a393b6ea444
HassanNawaz14 <hafizhassan142003@gmail.com>
2026-06-08 19:50:02 +0500

    Trigger Vercel rebuild: add document title to NotFound & move vercel.json into frontend

--- frontend/src/pages/NotFound.jsx
+++ frontend/src/pages/NotFound.jsx
@@ -1,7 +1,9 @@
 import { Link } from 'react-router-dom'
+import { useEffect } from 'react'
 import Header from '../components/Header.jsx'
 
 export default function NotFound() {
+  useEffect(() => { document.title = '404 - Stellar DNA' }, [])
   return (
     <div className="page">
       <Header />
